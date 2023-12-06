08b000564b3abae8e48931c1d86ca3f5af0eea31
beelzebub(amir) <amiriqbal@playsimple.in>
2023-12-06 22:44:02 +0530

    Created a spring boot application

---
+++ src/main/java/com/financeTracker/trackerAPIService/TrackerApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.financeTracker.trackerAPIService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerApiServiceApplication.class, args);
+	}
+
+}
